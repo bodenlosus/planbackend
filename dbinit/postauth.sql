c6c90f5ceb626f64398c80111c9de99db09a2162
bodenlosus <johannes.till.schmidt@outlook.de>
2025-09-14 19:25:20 +0200

    a

--- dbinit/postauth.sql
+++ dbinit/postauth.sql
@@ -3,3 +3,15 @@ CREATE POLICY read_acces_all FOR SELECT ON depots.depots USING (true) ;
 CREATE POLICY read_acces_for_owners FOR SELECT ON depots.positions USING ((SELECT auth.uid () AS uid) IN depots.depots.users) ;
 CREATE POLICY read_acces_for_owners FOR SELECT ON depots.positions USING ((SELECT auth.uid () AS uid) IN depots.depots.users) ;
 
+CREATE OR REPLACE FUNCTION new_depot_for_user() RETURNS TRIGGER AS $$
+    BEGIN
+        INSERT INTO depots.depots (created, cash, users)
+        VALUES (NOW(), 50000, ARRAY[NEW.id]);
+
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER new_user
+    AFTER INSERT ON auth.users
+    FOR EACH ROW EXECUTE FUNCTION new_depot_for_user();
