54341e8103ad94c9179ecf954a482c8901d9a4f0
Johannes <johannes.till.schmidt@outlook.de>
2025-09-14 21:28:34 +0200

    a

--- dbinit/postauth.sql
+++ dbinit/postauth.sql
@@ -1,7 +1,7 @@
 
-CREATE POLICY read_acces_all FOR SELECT ON depots.depots USING (true) ;
-CREATE POLICY read_acces_for_owners FOR SELECT ON depots.positions USING ((SELECT auth.uid () AS uid) IN depots.depots.users) ;
-CREATE POLICY read_acces_for_owners FOR SELECT ON depots.positions USING ((SELECT auth.uid () AS uid) IN depots.depots.users) ;
+CREATE "POLICY read_acces_all FOR" SELECT ON depots.depots USING (true) ;
+CREATE "POLICY read_acces_for_owners" FOR SELECT ON depots.positions USING ((SELECT auth.uid () AS uid) IN depots.depots.users) ;
+CREATE "POLICY read_acces_for_owners" FOR SELECT ON depots.positions USING ((SELECT auth.uid () AS uid) IN depots.depots.users) ;
 
 CREATE OR REPLACE FUNCTION new_depot_for_user() RETURNS TRIGGER AS $$
     BEGIN
